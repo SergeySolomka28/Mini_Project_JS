d79c403530244c4a7aa052aff34f085ed25ff727
SeregaSolomka28 <1994solomka@gmail.com>
2023-03-16 23:46:11 +0200

    qq

--- Project_JS1/script_user-details.js
+++ Project_JS1/script_user-details.js
@@ -4,8 +4,13 @@
 // (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
 //     6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
 
-let url =new URL(location.href)
+let url =new URL(window.location.href)
 let userUrl= url.searchParams.get('data')
 let userInfo =JSON.parse(userUrl);
 console.log(userInfo);
 console.log(JSON.stringify(userInfo));
+for (const userItem of userInfo) {
+
+    console.log(userItem)
+
+}
