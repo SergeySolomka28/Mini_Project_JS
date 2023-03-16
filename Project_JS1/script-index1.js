4700b0676d7785c38f7ef39a9de871b0f72f2593
SeregaSolomka28 <1994solomka@gmail.com>
2023-03-16 23:36:01 +0200

    Initial commit

---
+++ Project_JS1/script-index1.js
@@ -0,0 +1,52 @@
+// В index.html
+// 1 отримати масив об'єктів з endpoint`а https://jsonplaceholder.typicode.com/users
+// 2 Вивести id,name всіх user в index.html. Окремий блок для кожного user.
+// 3 Додати кожному блоку кнопку/посилання , при кліку на яку відбувається перехід  на сторінку user-details.html, котра має детальну інфорацію про об'єкт на який клікнули
+//
+//
+// На странице user-details.html:
+// 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
+// 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів поточного юзера
+// (для получения постов используйте эндпоинт https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+//     6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на сторінку post-details.html, котра має детальну інфу про поточний пост.
+//
+//     На странице post-details.html:
+// 7 Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
+// 8 Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт  - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)
+//
+// Стилизація проєкта -
+// index.html - всі блоки з user - по 2 в рядок. кнопки/аосилвння розташувати під інформацією про user.
+//     user-details.html - блок з інфою про user зверху сторінки. Кнопка нижчє, на 90% ширини сторінки, по центру.
+//     блоки з короткою іфною про post - в ряд по 5 .
+//     post-details.html - блок з інфою про пост зверху. Коментарі - по 4 в ряд.
+//     Всі елементи котрі характеризують users, posts, comments візуалізувати, так, щоб було видно що це блоки (дати фон. марджини і тд)
+
+let arrObject = fetch('https://jsonplaceholder.typicode.com/users')
+    .then(object => object.json())
+    .then(object => {
+        console.log(object);
+        for (const objectElement of object) {
+            let objectWrap = document.createElement('div')
+            objectWrap.classList.add('objectWrap')
+            let id = document.createElement('h2')
+            let name = document.createElement('h2')
+            id.innerText = ` Id: ${ objectElement.id}`
+            name.innerText = `Name: ${objectElement.name}`
+            let but1= document.createElement("button")
+            let a =document.createElement('a')
+            but1.classList.add('but1')
+            a.innerText='User Details'
+            a.href='user-details.html?data='+JSON.stringify(objectElement)
+            but1.appendChild(a)
+
+            objectWrap.append(id, name, but1);
+            let wrap = document.getElementById('wrap');
+            wrap.append(objectWrap);
+            document.body.appendChild(wrap);
+        }
+
+    })
+
+
+
+
